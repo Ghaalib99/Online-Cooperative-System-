3242fa1801b1c435df95f4ab55c95113e0d8c835
Saheed Olayioye <olaolajide99@gmail.com>
2023-07-17 18:56:49 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import Loans from "./pages/Loans";
+import AssetFinancing from "./pages/AssetFinanancing";
+import Investment from "./pages/Investment";
 
 
 function App() {
   return (
-    <>
+    <div className="relative">
     <Routes>
       <Route path="/" exact element={<Landing />} />
       <Route path="/register"  element={<Signup />} />
       <Route path="/login"  element={<Login />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/loans" element={<Loans />} />
+      <Route path="/asset-financing" element={<AssetFinancing />} />
+      <Route path="/investments" element={<Investment />} />
     </Routes>
       
-    </>
+    </div>
   );
 }
 
--- src/pages/Loans.jsx
+++ src/pages/Loans.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import cooperative1 from "../assets/cooperative1.jpg";
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Engage from "../components/Engage";
+import land4 from "../assets/land4.png";
+import land2 from "../assets/land2.jpg";
+import loan2 from "../assets/loan2.jpg";
+import loan3 from "../assets/loan3.jpg";
 
 const Loans = () => {
   return (
-    <div>Loans</div>
-  )
-}
+    <>
+      <Navbar />
+      
 
-export default Loans
+      <div className="w-full  lg:grid grid-cols-2 h-[100vh]">
+        <div className=" h-full ">
+          <img src={loan3} alt="" className=" w-full h-full object-cover " />
+        </div>
+        <div className="bg-light bg-opacity-90 text-center p-6 text-white flex justify-center items-center flex-col">
+          <h3 className="font-bold text-2xl w-[70%] my-4">Luxe Online Cooperative Platform grants you access to loans that are payable over a long period of time at very low interests.</h3>
+          <p className="w-[70%] text-xl">Loans are granted based on the credit limit of each individual Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia magni nihil ratione.</p>
+          <button className="bg-primary font-bold w-[200px] h-[50px] my-6 rounded-xl">Apply Now</button>
+        </div>
+      </div>
+
+      <footer
+        className="w-full bg-[#0e2721] text-center text-white text-xl leading-8 p-4"
+        style={{ fontFamily: "Raleway" }}
+      >
+        <p className="mb-6 font-bold">LUXE. COPYRIGHT@2023</p>
+      </footer>
+    </>
+  );
+};
+
+export default Loans;
